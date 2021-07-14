552b00c219661f67dbb266f887643384f77efd34
kamal9399 <66309049+kamal9399@users.noreply.github.com>
2021-07-14 11:15:24 +0530

    Add files via upload

---
+++ Ladderophilia.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+         int n;
+         cin >> n;
+
+         while(n--)
+         {
+                  cout<<"*   *"<<endl;
+                   cout<<"*   *"<<endl;
+                    cout<<"*****"<<endl;
+         }
+
+          cout<<"*   *"<<endl;
+           cout<<"*   *"<<endl;
+           return 0;
+
+}
---
+++ palindromicString.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+         string s;
+         cin>>s;
+
+         string str_rev;
+
+         for(int i = s.size()-1; i >= 0; --i )
+         {
+                  str_rev.push_back(i);
+         }
+
+         if(s== str_rev)
+         {
+                  cout<<"YES"<<endl;
+         }
+
+         else{
+                  cout<<"NO"<<endl;
+         }
+
+         return 0;
+}
